23039498a3849da2f227b25f5387a1bd61bb59ca
ianchuk <kirill.ianchuk@merck.com>
2020-11-17 22:12:52 +0300

    Logged each row in csv file during stream processing instead of accumulating in array

--- import-service/src/handlers/importFileParser.ts
+++ import-service/src/handlers/importFileParser.ts
@@ -9,21 +9,21 @@ const importAndParseFileFromS3 = (
     bucket: string,
     objectKey: string,
     s3: AWS.S3,
-): Promise<{[key: string]: string}[]> => new Promise((resolve, reject) => {
+): Promise<void> => new Promise((resolve, reject) => {
     const params = {
         Bucket: bucket,
         Key: objectKey
     };
-
-    const results = [];
 
     s3.getObject(params)
         .createReadStream()
         .on('error', reject)
         .pipe(csv())
         .on('error', reject)
-        .on('data', (data) => results.push(data))
-        .on('end', () => resolve(results))
+        .on('data', (data) => {
+            console.log(JSON.stringify(data, null, 2))
+        })
+        .on('end', resolve)
 });
 
 const moveFileInS3 = async (
@@ -50,13 +50,11 @@ export const importFileParser: S3Handler = async (event) => {
     const s3 = new AWS.S3({ region: config.aws.region });
 
     for (const record of event.Records) {
-        const products = await importAndParseFileFromS3(
+        await importAndParseFileFromS3(
             config.aws.s3.bucket,
             record.s3.object.key,
             s3,
         );
-
-        products.forEach(product => console.log(JSON.stringify(product, null, 2)));
 
         await moveFileInS3(
             config.aws.s3.bucket,
