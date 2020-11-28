9291a14da53287eba0a9a45d6d353e61b64d61e0
ianchuk <kirill.ianchuk@merck.com>
2020-11-28 14:56:05 +0300

    Configured CORS in GET /import endpoint

--- import-service/serverless.ts
+++ import-service/serverless.ts
@@ -46,6 +46,28 @@ const serverlessConfiguration: Serverless = {
       SQS_URL: `\$\{cf:${productServiceStackName}.SQSUrl\}`
     },
   },
+  resources: {
+    Resources: {
+      ApiGatewayRestApi: {
+        Type: 'AWS::ApiGateway::RestApi',
+        Properties: {
+          Name: '${self:service}-${self:provider.stage}'
+        }
+      },
+      GatewayResponseDefault4XX: {
+        Type: 'AWS::ApiGateway::GatewayResponse',
+        Properties: {
+          ResponseParameters: {
+            'gatewayresponse.header.Access-Control-Allow-Origin': '\'*\''
+          },
+          ResponseType: 'DEFAULT_4XX',
+          RestApiId: {
+            Ref: 'ApiGatewayRestApi'
+          }
+        }
+      }
+    }
+  },
   functions: {
     'import-products-file': {
       handler: 'handler.importProductsFile',
@@ -67,6 +89,10 @@ const serverlessConfiguration: Serverless = {
               arn: 'arn:aws:lambda:#{AWS::Region}:#{AWS::AccountId}:function:authorization-service-dev-basic-authorizer',
               resultTtlInSeconds: 0,
               identitySource: 'method.request.header.Authorization'
+            },
+            cors: {
+              origins: '*',
+              headers: ['Authorization']
             }
           },
         },
